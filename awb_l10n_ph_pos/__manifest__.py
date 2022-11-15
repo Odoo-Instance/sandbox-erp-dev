f5abe5cb0194010b4fb0e549deaa374b70b4a9e9
Marianne Cyra Jun R. Quinzon <marianne.quinzon@achievewithoutborders.com>
2022-11-15 16:16:56 +0800

    Revert "[FIX] fixed missing dependencies."
    This reverts commit 18dc753c19ec78cc5782737ca3ee4ec63e840969.

--- awb_l10n_ph_pos/__manifest__.py
+++ awb_l10n_ph_pos/__manifest__.py
@@ -20,7 +20,7 @@
     # any module necessary for this one to work correctly
     'depends': [
         'point_of_sale',
-        'point_of_sale.models',
+        'product'
     ],
 
     # always loaded
