d9076bb5310c16f059f3a38a9af5624b2bb2e82b
bhavita <bhavitta.bhatt@achievewithoutborders.com>
2022-11-24 15:46:08 +0530

    pos void order journal entries

---
+++ pos_sale_receipt/models/crm_team.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+
+
+class CrmTeam(models.Model):
+    _inherit = 'crm.team'
+
+    starting_sequence_number = fields.Integer(string='Starting Sequence number')
+    current_sequence_number = fields.Integer(string='Current Sequence number', compute ='onchange_starting_sequence_number', store=True)
+    ending_sequence_number = fields.Integer(string='Ending Sequence number', store=True)
+    threshold_sequence_number = fields.Integer(string='Threshold Sequence number')
+    sale_team_prefix_id = fields.Many2one(string="Sale Team Prefix", comodel_name='sale.team.prefix', copy=False)
+
+    @api.depends('ending_sequence_number')
+    def onchange_ending_sequence_number(self):
+        for record in self:
+            if record.ending_sequence_number < record.current_sequence_number:
+                raise UserError(_('Ending sequence number should be greater than current sequence number.'))
+
+    @api.depends('starting_sequence_number')
+    def onchange_starting_sequence_number(self):
+        for record in self:
+            record.current_sequence_number = record.starting_sequence_number
---
+++ pos_sale_receipt/models/sale_team_prefix.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api, _
+
+class SaleTeamPrefix(models.Model):
+    _name = "sale.team.prefix"
+
+    name = fields.Char(string='Sales Team Prefix for sequence', size=3, copy=False)
+
+    _sql_constraints = [
+    ('sales_team_prefix_uniq', 'unique (name)',
+        " A sales team already exists with same prefix. Please choose another prefix.")
+    ]
