8a8a8df495f6f94b3184d27cc8ee80c24ba98f02
awb-Tony <tony.saji@achievewithoutborders.com>
2022-12-28 07:45:44 +0530

    pos [Fix]As a POS admin, I should be allowed to configure settings for POS

--- pos_sale_receipt/models/crm_team.py
+++ pos_sale_receipt/models/crm_team.py
@@ -2,14 +2,18 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import MissingError, UserError, ValidationError, AccessError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class CrmTeam(models.Model):
     _inherit = 'crm.team'
 
     starting_sequence_number = fields.Integer(string='Starting Sequence number')
-    current_sequence_number = fields.Integer(string='Current Sequence number', compute ='onchange_starting_sequence_number', store=True)
+    current_sequence_number = fields.Integer(string='Current Sequence number',
+                                             compute='onchange_starting_sequence_number', store=True)
     ending_sequence_number = fields.Integer(string='Ending Sequence number', store=True)
     threshold_sequence_number = fields.Integer(string='Threshold Sequence number')
     sale_team_prefix_id = fields.Many2one(string="Sale Team Prefix", comodel_name='sale.team.prefix', copy=False)
@@ -24,3 +28,29 @@ class CrmTeam(models.Model):
     def onchange_starting_sequence_number(self):
         for record in self:
             record.current_sequence_number = record.starting_sequence_number
+
+
+    # this is for to change of starting_sequence_number
+    @api.onchange('awb_pos_provider_is_training_mode')
+    def onchange_awb_pos_provider_is_training_mode(self):
+        if self.awb_pos_provider_is_training_mode:
+            self.sale_team_prefix_id = self.env['sale.team.prefix'].search([('name', '=', 'TES')], limit=1)
+            print(self.sale_team_prefix_id, "sale_team_prefix_id")
+        else:
+            self.sale_team_prefix_id = self.env['sale.team.prefix']
+
+    # def action_pos_order_invoice(self):
+    #     if self.awb_pos_provider_is_training_mode:
+    #         return super().action_pos_order_invoice()
+    #     if not self.env.user.is_admin:
+    #         raise AccessError("Only admin users can access this flag")
+
+    # rest of the method implementation goes here
+    # def awb_pos_provider_is_training_mode(self):
+    #     if not self.env.user.is_admin:
+    #         raise AccessError("Only admin users can access this flag")
+
+    # def awb_pos_provider_is_training_mode(self):
+    #     if self.env.user.is_admin:
+    #         if self.awb_pos_provider_is_training_mode is None:
+    #             self.awb_pos_provider_is_training_mode = False
